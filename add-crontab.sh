453e7bd554f1106e5ce387e05ce33f9d5b69a4bb
hymvp <hanyongmvp@163.com>
2023-06-10 21:31:12 +0800

    Create add-crontab.sh

---
+++ add-crontab.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# 设置要执行的脚本和定时规则
+SCRIPT="/home/opc/metrics-publish-corntab.sh"
+CRON_SCHEDULE="* * * * *"
+
+# 将新的定时任务添加到临时文件中
+echo "$CRON_SCHEDULE $SCRIPT" >> temp_crontab
+
+# 将临时文件中的内容导入到 crontab 中
+crontab temp_crontab
+
+# 删除临时文件
+rm temp_crontab
